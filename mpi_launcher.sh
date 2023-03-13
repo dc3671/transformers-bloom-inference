cd58842c1de02e9962a306b0dcca638e999c2604
baodii <di.bao@intel.com>
2023-03-13 02:48:45 -0700

    add bf16 cpu support

--- mpi_launcher.sh
+++ mpi_launcher.sh
@@ -1,1 +1,1 @@
-mpirun -np 1 ipdb3 -m inference_server.benchmark --model_name bigscience/bloom-560m --model_class AutoModelForCausalLM --dtype fp16 --deployment_framework ds_inference --benchmark_cycles 5
+deepspeed --num_gpus 1 --module inference_server.benchmark --model_name bigscience/bloom-560m --model_class AutoModelForCausalLM --dtype bf16 --deployment_framework ds_inference --benchmark_cycles 5
